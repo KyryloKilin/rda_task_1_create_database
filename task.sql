258f69dc904d3252b7856d54372aef7b3870fb66
root <root@vmi2772850.contaboserver.net>
2025-11-18 09:30:03 +0000

    Use ID as primary key and fix foreign keys in ShopDB schema

--- task.sql
+++ task.sql
@@ -4,41 +4,41 @@ USE ShopDB;
 
 
 CREATE TABLE Products (
-    ProductID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Description VARCHAR(100),
     Price INT,
     WarehouseAmount INT,
-    PRIMARY KEY (ProductID)
+    PRIMARY KEY (ID)
 );
 
 
 CREATE TABLE Customers (
-    CustomerID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     Email VARCHAR(50),
     Address VARCHAR(100),
-    PRIMARY KEY (CustomerID)
+    PRIMARY KEY (ID)
 );
 
 
 CREATE TABLE Orders (
-    OrderID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT,
     CustomerID INT,
     Date DATE,
-    PRIMARY KEY (OrderID),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) ON DELETE SET NULL
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
 );
 
 
 CREATE TABLE OrderItems (
-    OrderItemID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT,
     OrderID INT,
     ProductID INT,
-    PRIMARY KEY (OrderItemID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID) ON DELETE SET NULL,
-    FOREIGN KEY (ProductID) REFERENCES Products(ProductID) ON DELETE SET NULL
+    PRIMARY KEY (ID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
 );
 
 
